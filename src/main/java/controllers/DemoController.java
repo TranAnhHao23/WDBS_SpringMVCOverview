bb24b746f25d90b27d0b335a987e58904a212d9f
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-02-18 13:33:52 +0700

    add ex1

---
+++ src/main/java/controllers/DemoController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DemoController {
+//    @GetMapping("/greeting")
+//    public String greeting(){
+//        return "view";
+//    }
+    @GetMapping("/greeting")
+    public ModelAndView changeName(@RequestParam String name) {
+        ModelAndView modelAndView = new ModelAndView("view");
+        modelAndView.addObject("name", name);
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/views/view.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 18-Feb-22
+  Time: 11:53 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello Đức óc chó</h1>
+<h1>Hello ${name} óc chó</h1>
+</body>
+</html>
